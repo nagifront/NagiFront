05dacbd75d00283a1300db3fd17013c4846aa45b
Leeingnyo <dyd1928@naver.com>
2016-04-24 02:59:27 +0900

    add comments

--- NagiFront/nagifront/nagifront_router.py
+++ NagiFront/nagifront/nagifront_router.py
@@ -2,20 +2,33 @@ import re
 
 class NagifrontRouter(object):
     def db_for_read(self, model, **hints):
+        """
+        The model which has prefix 'nagios', read from nagios database
+        """
         if re.match('^nagios.*', model._meta.model_name) is not None:
+        # nagios로 시작하는 테이블이면 nagios db에서
             return 'nagios'
         return 'default'
 
     def db_for_write(self, model, **hints):
+        """
+        No write permission on nagios database
+        """
         return 'default'
 
     def allow_relation(self, obj1, obj2, **hints):
+        """
+        No Relations on nagios database
+        """
         if re.match('^nagios.*', obj1._meta.model_name) is not None \
                 or re.match('^nagios.*', obj2._meta.model_name) is not None:
             return False
         return True
 
     def allow_migrate(self, db, app_label, model_name=None, **hints):
+        """
+        No migrate permission on nagios database
+        """
         if db == 'nagios':
             return False
         return True
