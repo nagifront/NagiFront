7c0cfdd0402c8b5a33324ce088657b1f762d55bf
Leeingnyo <dyd1928@naver.com>
2016-04-24 02:52:45 +0900

    add our app to settings

--- NagiFront/NagiFront/settings.py
+++ NagiFront/NagiFront/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'nagifront.apps.NagifrontConfig',
 ]
 
 MIDDLEWARE_CLASSES = [
