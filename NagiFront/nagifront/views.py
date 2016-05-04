264072a68dbfd1c5d9b8f3886538a9969bfbe50d
Myrmidon <myrmidonxd@gmail.com>
2016-05-05 03:17:28 +0900

    Implemented login view

--- NagiFront/nagifront/views.py
+++ NagiFront/nagifront/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login as django_login
 from .models import UserProfile
 
 # Create your views here.
@@ -10,8 +11,23 @@ def index(request):
     return HttpResponse("Hello, world. You're at the nagifront index.")
 
 def login(request):
-    # 이미 되어있는가 보기?
-    # / 로 리다이렉트
-    # 포스트로 처리
+    message = None
+    if request.user.is_authenticated():
+        return redirect('index')
+
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                django_login(request, user)
+                return redirect('index')
+            else:
+                message = "This user is disabled!"
+        else:
+            message = "Username or password is invalid."
+
     return render(request, 'nagifront/login.html', {
+        'message' : message
     })
