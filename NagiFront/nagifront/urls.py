a1f036206410a2a212a007749ca5f892b366b82c
Myrmidon <myrmidonxd@gmail.com>
2016-05-04 22:50:04 +0900

    Deleted improper files and changes

--- NagiFront/nagifront/urls.py
+++ NagiFront/nagifront/urls.py
@@ -4,5 +4,4 @@ from . import views
 
 urlpatterns = [ 
         url(r'^$', views.index, name='index'),
-        url(r'^dashboard_setting/(?P<user_id>[0-9]+)/$', views.show_dashboard_setting, name='dashboard_setting'), 
         ]
--- NagiFront/nagifront/views.py
+++ NagiFront/nagifront/views.py
@@ -9,9 +9,3 @@ from .models import UserProfile
 def index(request):
     return HttpResponse("Hello, world. You're at the nagifront index.")
 
-# Test view for check dashboard setting functionality.
-def show_dashboard_setting(request, user_id):
-    selected_user = User.objects.get(pk=user_id)
-    context = {"selected_user_dashboard_setting": selected_user.userprofile.dashboard_setting }
-    return render(request, 'nagifront/dashboard_setting.html', context)
-
