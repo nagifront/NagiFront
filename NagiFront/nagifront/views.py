be3d2ee6e5b8cab4056d664353436b5f3deddd5c
Myrmidon <myrmidonxd@gmail.com>
2016-05-04 22:38:46 +0900

    Fixed indent in customfields.py

--- NagiFront/nagifront/views.py
+++ NagiFront/nagifront/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from django.contrib.auth.models import User
+from .models import UserProfile
+
 # Create your views here.
 
 def index(request):
     return HttpResponse("Hello, world. You're at the nagifront index.")
 
+# Test view for check dashboard setting functionality.
+def show_dashboard_setting(request, user_id):
+    selected_user = User.objects.get(pk=user_id)
+    context = {"selected_user_dashboard_setting": selected_user.userprofile.dashboard_setting }
+    return render(request, 'nagifront/dashboard_setting.html', context)
 
