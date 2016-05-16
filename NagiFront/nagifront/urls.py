9ccd76035286d555dd5a26f43ca3430f8a7ec380
Myrmidon <myrmidonxd@gmail.com>
2016-05-17 06:52:12 +0900

    Implemented API: GET /hosts/groups/trouble-trend
    - NagiFront/settings.py
      - Modified Timezone setting from 'UTC' to 'Asia/Seoul'
    - nagifront/urls.py
      - Added the url for API
    - nagifront/views.py
      - Implemented hosts_groups_trouble_trend view method for API

--- NagiFront/nagifront/urls.py
+++ NagiFront/nagifront/urls.py
@@ -13,6 +13,7 @@ urlpatterns = [
         url(r'^hosts/groups$', views.hosts_groups, name='hosts-groups'),
         url(r'^hosts/groups/hosts-state$', views.hosts_groups_hosts_state, name='hosts-groups-hosts-state'),
         url(r'^hosts/groups/service-number-by-state$', views.hosts_groups_service_number_by_state, name='host-groups-service-number-by-state'),
+        url(r'^hosts/groups/trouble-trend$', views.hosts_groups_trouble_trend, name='host-groups-trouble-trend'),
         
         url(r'^hosts/(?P<host_id>[0-9]+)/trend$', views.hosts_id_trend, name='hosts-id-trend'),
 
