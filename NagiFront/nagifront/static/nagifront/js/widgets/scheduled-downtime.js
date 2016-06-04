a625e0d96ed4eff2bc624222780e9daf4ec863c6
seyounglee <fatalu1226@naver.com>
2016-06-04 10:50:16 +0900

    able to get data from API

--- NagiFront/nagifront/static/nagifront/js/widgets/scheduled-downtime.js
+++ NagiFront/nagifront/static/nagifront/js/widgets/scheduled-downtime.js
@@ -5,13 +5,13 @@ angular.module('nagifront')
     scope: {
     },
     template: '<h3>다운타임 스케쥴</h3><table class="charts">'
-						+'<tr>'
-							+'<th>Host name</th>'
-							+'<th>Entry Time</th>'
-							+'<th>Comments</th>'
-							+'<th>Start Time</th>'
-							+'<th>End Time</th>'
-						+'</tr>'
+            +'<tr>'
+              +'<th>Host name</th>'
+              +'<th>Entry Time</th>'
+              +'<th>Comments</th>'
+              +'<th>Start Time</th>'
+              +'<th>End Time</th>'
+            +'</tr>'
         +'<tr class="downtimes" ng-repeat="downtime in downtimes">'
           +'<td><span>{{downtime.host_name}}</span></td>'
           +'<td>{{downtime.entry_time | date:"yyyy/MM/dd HH:mm:ss"}}</td>'
@@ -22,10 +22,9 @@ angular.module('nagifront')
       +'</table>',
       link: function(scope, element, attrs) {
         getData = function() {
- //         $http.get(djangoUrl.reverse('host-groups-service-number-by-state')).then(function(response) {
-   //         scope.data = response.data;
-        //});
-					scope.downtimes = [{"entry_time": "2016-05-03T20:58:49Z", "comment_data": "The General uses Shukuchi", "scheduled_start_time": "2016-05-03T21:58:49Z", "scheduled_end_time": "2016-05-03T22:58:49Z", "host_name":"uriel"}, {"entry_time": "2016-05-03T20:58:49Z", "comment_data": "The General uses Shukuchi lalalalala", "scheduled_start_time": "2016-05-03T21:58:49Z", "scheduled_end_time": "2016-05-03T22:58:49Z", "host_name":"uriel lalalalalala"}];
+          $http.get(djangoUrl.reverse('configuration-scheduled-downtime')).then(function(response) {
+          scope.downtimes = response.data.scheduled_downtime;
+        });
         }
         $interval(getData, 30000);
         getData();
