31affa77fface4ece00dab437df97f92abc1f714
Leeingnyo <dyd1928@naver.com>
2016-06-06 04:21:01 +0900

    fix typo

--- NagiFront/nagifront/static/nagifront/js/dashboard.js
+++ NagiFront/nagifront/static/nagifront/js/dashboard.js
@@ -1,8 +1,8 @@
 app.controller('dashboard', function($scope, $http, djangoUrl){
   $http.get(djangoUrl.reverse('hosts-ids')).then(function(response) {
-    scope.host_ids = response.data.ids;
+    $scope.host_ids = response.data.ids;
   });
   $http.get(djangoUrl.reverse('hosts-groups-ids')).then(function(response) {
-    scope.hostgroup_ids = response.data.ids;
+    $scope.hostgroup_ids = response.data.ids;
   });
 })
