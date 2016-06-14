57dd06e75e57ee4e0483917505f4433f84e61559
Leeingnyo <dyd1928@naver.com>
2016-06-14 23:44:10 +0900

    align indent

--- NagiFront/nagifront/static/nagifront/js/system.js
+++ NagiFront/nagifront/static/nagifront/js/system.js
@@ -1,41 +1,41 @@
 app.controller('system', function($scope, $http, djangoUrl){
- $scope.categories = ['Configuration'];
- $scope.divisions = [''];
- $scope.type = 'none';
- $scope.sections = [''];
- $scope.ready = false;
- $scope.isHosts = function() {
-   return $scope.type === 'Hosts';
- }
- $scope.isHostgroups = function() {
-   return $scope.type === 'Host groups';
- }
- $scope.isServices = function() {
-   return $scope.type === 'Services';
- }
- $scope.isContacts = function() {
-   return $scope.type === 'Contacts';
- }
- $scope.isContactgroups = function() {
-   return $scope.type === 'Contact groups';
- }
- $scope.isTimeperiods = function() {
-   return $scope.type === 'Time periods';
- }
- $scope.isCommands = function() {
-   return $scope.type === 'Commands';
- }
- $scope.show = function() {
-  $scope.ready = true;
-  $scope.type = $scope.division;
-  $scope.option = $scope.section;
- }
- $scope.update = function(value) {
-   if(value === 'Configuration') {
-     $scope.divisions = ['Hosts', 'Host groups', 'Services', 'Contacts', 'Contact groups', 'Time periods', 'Commands'];
-     $scope.category = value;
-   }
-   else if(value === 'Hosts') {
+  $scope.categories = ['Configuration'];
+  $scope.divisions = [''];
+  $scope.type = 'none';
+  $scope.sections = [''];
+  $scope.ready = false;
+  $scope.isHosts = function() {
+    return $scope.type === 'Hosts';
+  }
+  $scope.isHostgroups = function() {
+    return $scope.type === 'Host groups';
+  }
+  $scope.isServices = function() {
+    return $scope.type === 'Services';
+  }
+  $scope.isContacts = function() {
+    return $scope.type === 'Contacts';
+  }
+  $scope.isContactgroups = function() {
+    return $scope.type === 'Contact groups';
+  }
+  $scope.isTimeperiods = function() {
+    return $scope.type === 'Time periods';
+  }
+  $scope.isCommands = function() {
+    return $scope.type === 'Commands';
+  }
+  $scope.show = function() {
+   $scope.ready = true;
+   $scope.type = $scope.division;
+   $scope.option = $scope.section;
+  }
+  $scope.update = function(value) {
+    if(value === 'Configuration') {
+      $scope.divisions = ['Hosts', 'Host groups', 'Services', 'Contacts', 'Contact groups', 'Time periods', 'Commands'];
+      $scope.category = value;
+    }
+    else if(value === 'Hosts') {
       $http.get(djangoUrl.reverse('hosts-ids')).then(function(response) {
         var data = response.data.ids;
         $scope.sections = ['All'];
@@ -44,8 +44,8 @@ app.controller('system', function($scope, $http, djangoUrl){
         });
       });
       $scope.division = value;
-   }
-   else if(value === 'Host groups') {
+    }
+    else if(value === 'Host groups') {
       $http.get(djangoUrl.reverse('hosts-groups')).then(function(response) {
         var data = response.data;
         $scope.sections = [];
@@ -54,8 +54,8 @@ app.controller('system', function($scope, $http, djangoUrl){
         });
       });
       $scope.division = value;
-   }
-   else if(value === 'Services') {
+    }
+    else if(value === 'Services') {
       $http.get(djangoUrl.reverse('hosts-ids')).then(function(response) {
         var data = response.data.ids;
         $scope.sections = ['All'];
@@ -64,21 +64,21 @@ app.controller('system', function($scope, $http, djangoUrl){
         });
       });
       $scope.division = value;
-   }
-   else if(value === 'Contacts') {
-      $scope.sections = ['All'];
-   }
-   else if(value === 'Contact groups') {
-      $scope.sections = ['All'];
-   }
-   else if(value === 'Time periods') {
-      $scope.sections = ['All'];
-   }
-   else if(value === 'Commands') {
-      $scope.sections = ['All'];
-   }
-   else if(value !== '') {
-     $scope.section = value;
-   }
- };
+    }
+    else if(value === 'Contacts') {
+       $scope.sections = ['All'];
+    }
+    else if(value === 'Contact groups') {
+       $scope.sections = ['All'];
+    }
+    else if(value === 'Time periods') {
+       $scope.sections = ['All'];
+    }
+    else if(value === 'Commands') {
+       $scope.sections = ['All'];
+    }
+    else if(value !== '') {
+      $scope.section = value;
+    }
+  };
 })
