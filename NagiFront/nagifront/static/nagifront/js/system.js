8c65682f636b4245135cbb0c606b5a40ecaa4d04
seyounglee <fatalu1226@naver.com>
2016-06-14 12:23:56 +0900

    implementing option bar

--- NagiFront/nagifront/static/nagifront/js/system.js
+++ NagiFront/nagifront/static/nagifront/js/system.js
@@ -1,3 +1,12 @@
 app.controller('system', function($scope, $http, djangoUrl){
-
+ $scope.categories = ['Configuration'];
+ $scope.divisions = [''];
+ $scope.sections = [''];
+ $scope.update = function() {
+  if($scope.category === 'configuration') {
+    $scope.divisions = ['Hosts', 'Host group', 'Service'];
+  }
+  if($scope.division === 'Hosts') {
+  }
+ }
 })
