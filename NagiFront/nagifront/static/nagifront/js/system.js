e02496fc75f7bafcc690399b4f59e101b21d0c70
seyounglee <fatalu1226@naver.com>
2016-06-14 14:30:29 +0900

    implement option bar

--- NagiFront/nagifront/static/nagifront/js/system.js
+++ NagiFront/nagifront/static/nagifront/js/system.js
@@ -2,11 +2,33 @@ app.controller('system', function($scope, $http, djangoUrl){
  $scope.categories = ['Configuration'];
  $scope.divisions = [''];
  $scope.sections = [''];
- $scope.update = function() {
-  if($scope.category === 'configuration') {
-    $scope.divisions = ['Hosts', 'Host group', 'Service'];
-  }
-  if($scope.division === 'Hosts') {
-  }
- }
+ $scope.update = function(value) {
+   if(value === 'Configuration') {
+     $scope.divisions = ['Hosts', 'Host group', 'Service'];
+     $scope.category = value;
+   }
+   else if(value == 'Hosts') {
+      $http.get(djangoUrl.reverse('hosts-ids')).then(function(response) {
+        var data = response.data.ids;
+        $scope.sections = [];
+        angular.forEach(data, function(value, key) {
+          $scope.sections.push(key);
+        });
+      });
+      $scope.division = value;
+   }
+   else if(value == 'Host group') {
+      $http.get(djangoUrl.reverse('hosts-groups')).then(function(response) {
+        var data = response.data;
+        $scope.sections = [];
+        angular.forEach(data, function(value, key) {
+          $scope.sections.push(value.alias);
+        });
+      });
+      $scope.division = value;
+   }
+   else if(value != '') {
+     $scope.final_value = value;
+   }
+ };
 })
