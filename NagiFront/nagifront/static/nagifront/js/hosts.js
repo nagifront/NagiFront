222ac7cb5ae77749562567d00004a64d107cff4d
Leeingnyo <dyd1928@naver.com>
2016-06-14 15:17:50 +0900

    fix indent fix boolean

--- NagiFront/nagifront/static/nagifront/js/hosts.js
+++ NagiFront/nagifront/static/nagifront/js/hosts.js
@@ -1,16 +1,16 @@
 app.controller('hosts', function($scope, $http, $window, djangoUrl){
   $scope.groups= [];
-  $scope.ready = 0;
+  $scope.ready = false;
   $scope.selected = {name: undefined, id: -1};
   $http.get(djangoUrl.reverse('hosts-groups')).then(function(response) {
     var data = response.data;
-      angular.forEach(data, function(value, key) {
-        $scope.groups.push({name: value.alias, id: key});
+    angular.forEach(data, function(value, key) {
+      $scope.groups.push({name: value.alias, id: key});
     });
   });
   $scope.selectGroup = function(group) {
     $scope.selected = group;
-    $scope.ready = 1;
+    $scope.ready = true;
   };
   $scope.move = function(id) {
     $http.get(djangoUrl.reverse('search')+'&id='+id+'&type=host')
