c9d6c6e5782ea126ca93199066548437f6c1aa8e
Leeingnyo <dyd1928@naver.com>
2016-06-17 15:27:49 +0900

    fix bug when adding or deleting widget

--- NagiFront/nagifront/static/nagifront/js/widgets/map.js
+++ NagiFront/nagifront/static/nagifront/js/widgets/map.js
@@ -193,7 +193,6 @@ angular.module('nagifront')
           if (is_first){
             var map_wrapper = jQuery(element[0]).find('.map-wrapper');
             // FIXME use angular
-            console.log(element[0])
             map_wrapper.scrollTop = width / 2 - (jQuery(element[0]).find('.map-wrapper').offsetWidth / 2)
             map_wrapper.scrollLeft = height / 2 - (jQuery(element[0]).find('.map-wrapper').offsetHeight /2 )
             is_first = false;
